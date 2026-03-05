9b46d31417358da72e13c60cd33fa7c5838adb10
henry753951 <henryboy753951@gmail.com>
2026-03-06 02:59:21 +0800

    feat: add @types/node as a dependency and update security settings in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,9 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
 	output: "server",
+	security: {
+		checkOrigin: false,
+	},
 	adapter: node({
 		mode: "standalone",
 	}),
